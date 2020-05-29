14160112ec46a4939ff9e58baff6f1d8474a0a98
Karla Magaña <65092711+karlamagana@users.noreply.github.com>
2020-05-29 12:00:41 -0600

    Add files via upload

---
+++ administrador/mantenimiento.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mantenimiento</title>
+	<link rel="stylesheet" type="text/css" href="../css/index.css">
+    <link rel="stylesheet" type="text/css" href="../css/card.css">
+</head>
+<body>
+<?php 
+    require_once('menuadmin.php');  
+?>
+
+<section >
+    	<br>
+    	<br>
+<center>
+     <table>
+		<tr>
+			<td>
+		<div class="container">
+		<div class="card">
+			<a href="viewservicios.php"><img src="../fotos/sevicios.jpg"></a>	
+			<br>
+		<h4><a href="viewservicios.php">Tus Sevicios</a> </h4>
+		</div>		
+	    </div>
+			</td>
+	
+
+		
+			<td>
+		<div class="container">
+		<div class="card">
+			<a href="ingresousuarios.php"><img src="../fotos/usuario.png"></a>	
+			<br>
+		<h4><a href="ingresousuarios.php">Usuarios</a> </h4>
+		</div>		
+	    </div>
+			</td>
+		
+
+		
+			<td>
+		<div class="container">
+		<div class="card">
+			<a href="viewservicios.php"><img src="../fotos/catalogo.png"></a>	
+			<br>
+		<h4><a href="">Catalogo</a> </h4>
+		</div>		
+	    </div>
+			</td>
+		</tr>
+
+	</table>
+</center>
+    	
+  	
+    </section>
+
+    <br>
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+    	<br>
+<?php
+require_once("../include/pie.php");
+?>
+</body>
+</html>
