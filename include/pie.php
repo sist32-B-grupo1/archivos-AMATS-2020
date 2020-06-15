3062cf2fe3436119dd903efb9e194d85af8300d6
Karla Magaña <65092711+karlamagana@users.noreply.github.com>
2020-06-15 11:24:10 -0600

    Add files via upload

---
+++ include/pie.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+<body>
+<footer style="clear: both;
+text-align: left;
+padding: 40px;
+color: #343749;
+background: #fe3686; ">
+  	<h4>Derechos reservados &copy: 2020</h4>
+    <h4 style="text-align: right;">YA TENEMOS LOCAL</h4>
+    <p style="text-align: right;">Estamos ubicados en 9av. Sur entre calle libertad<br>oriente y 1 calle oriente, por<br> floristeria magnolia, sector san juan de Dios</p>
+  	<div class="social-menu">
+  <ul>
+    <li><a href="https://m.facebook.com/JacquiieMontano/"><i class="fa fa-facebook"></i></a></li>
+    <li><a href="https://www.instagram.com/aries160412/?hl=es-la"><i class="fa fa-instagram"></i></a></li>
+  </ul>
+</div>
+  </footer>
+</body>
+</html>
+
+<style type="text/css">
+.social-menu ul {
+    position: absolute;
+
+    left: 50%;
+    transform: translate(-50%, -60%);
+    padding: 0;
+    margin: 0;
+    display: flex;
+}
+.social-menu ul li {
+    list-style: none;
+    margin: 0 10px;
+}
+.social-menu ul li .fa {
+    color: #000000;
+    font-size: 25px;
+    line-height: 50px;
+    transition: .5s;
+}
+.social-menu ul li .fa:hover {
+    color: #ffffff;
+}
+.social-menu ul li a {
+    position: relative;
+    display: block;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-color: white;
+    text-align: center;
+    transition: 0.5s;
+    transform: translate(0,0px);
+    box-shadow: 0px 7px 5px rgba(0, 0, 0, 0.5);
+}
+.social-menu ul li a:hover {
+    transform: rotate(0deg) skew(0deg) translate(0, -10px);
+}
+.social-menu ul li:nth-child(1) a:hover {
+    background-color: #3b5999;
+}
+.social-menu ul li:nth-child(2) a:hover {
+    background-color: #55acee;
+}
+.social-menu ul li:nth-child(3) a:hover {
+    background-color: #e4405f;
+}
+.social-menu ul li:nth-child(4) a:hover {
+    background-color: #cd201f;
+}
+.social-menu ul li:nth-child(5) a:hover {
+    background-color: #0077B5;
+}
+</style>
