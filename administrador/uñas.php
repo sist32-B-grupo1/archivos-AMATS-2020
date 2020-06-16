a120f69291be39dbe4d1d7bae9055ddec09778df
FatimaGuerra98 <65093538+FatimaGuerra98@users.noreply.github.com>
2020-06-15 21:23:57 -0600

    Add files via upload

---
+++ administrador/uñas.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <title>Uñas</title>
+
+     <link rel="stylesheet" href="doc.css">
+</head>
+<?php
+require_once("menuadmin.php");
+?>
+<body>
+    <p>CATÁLOGO</p><br><br><br>
+    <center>
+    <a href="catalogo.php" onclick="mostrar('Regresar'); return false" />Regresar</a><br><br><br>
+    </center><br><br><br>
+<table>
+
+<tr ><center>
+   <td align="center">$5.60</td> 
+
+   <td align="center">$12.40</td>
+
+   <td align="center">$14.30</td></center>
+</tr>
+<tr>
+    
+   <td align="center"><img src="fotoscat/uñas1.jpg" ></td>
+   
+   <td align="center"><img src="fotoscat/uñas2.jpg"></td>
+
+   <td align="center"><img src="fotoscat/uñas3.jpg"></td>
+</tr>
+<tr >
+   <td align="center">Con brillo</td> 
+   <td align="center">Francesas</td> 
+   <td align="center">Decoradas</td> 
+
+</tr>
+<tr >
+   <td align="center"><input type="submit" name="eliminar" value="eliminar"/></td> 
+   <td align="center"><input type="submit" name="eliminar" value="eliminar"/></td> 
+   <td align="center"><input type="submit" name="eliminar" value="eliminar"/></td>
+   </tr> 
+</table><br><br><br>
+
+</body>
+</html>
+
+
+<?php
+require_once("../include/pie.php");
+?>
