a6869248778699e71aaeba1ed6d58e6fc2929004
FatimaGuerra98 <65093538+FatimaGuerra98@users.noreply.github.com>
2020-06-18 09:24:29 -0600

    Add files via upload

---
+++ clases/conexion.php
@@ -0,0 +1,9 @@
+<?php
+
+include "configuracion.php";
+$conexion = new mysqli($server, $user, $pass, $bd);
+
+if (mysqli_connect_errno()) {
+    echo "No Conectado", mysqli_connect_error();
+    exit();
+}
